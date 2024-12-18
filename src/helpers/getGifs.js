20046ec9814d7541fa28e853c3f417b2a9c16057
Juan Maldonado <juan.maldonado@live.cl>
2024-12-18 19:14:26 -0300

    fix: correct url for api

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,11 +1,14 @@
 export const getGifs = async (category) => {
   
-  const url = `/api/gifs?category:${category}`
+  const url = `/api/gifs?category=${category}`
   const response = await fetch(url);
   
   const gifs = await response.json()
   return gifs;
-  // const apiKey = import.meta.env.VITE_GIF_KEY_TENOR;
+ 
+};
+
+ // const apiKey = import.meta.env.VITE_GIF_KEY_TENOR;
   // const clientkey = "Gif-app";
   // const lmt = 10;
   // const url = `https://tenor.googleapis.com/v2/search?q=${category}&key=${apiKey}&client_key=${clientkey}&limit=${lmt}`;
@@ -16,4 +19,3 @@ export const getGifs = async (category) => {
   //   title: img.tags[0],
   //   url: img.media_formats.gif.url,
   // }));
-};
